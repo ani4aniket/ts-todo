55b4acd949f9286ab8e32758a6c77483f43871ee
Aniket Kumar <aniketkumar049@gmail.com>
2020-03-11 23:38:32 +0530

    props and types for props added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
+
+  const todos = [{ id: 't1', text: 'Gym workout'}];
+
   return (
     <div className="App">
-      <h2>HEllo</h2>
+      <TodoList items={todos}/>
     </div>
   )
 }
