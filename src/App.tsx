0ca3836aa850c3b52defcb0cb6fb83529fa12c76
Aniket Kumar <aniketkumar049@gmail.com>
2020-03-12 00:36:58 +0530

    cross component connection

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,13 @@ const App: React.FC = () => {
 
   const todos = [{ id: 't1', text: 'Gym workout'}];
 
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  }
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler}/>
       <TodoList items={todos}/>
     </div>
   )
